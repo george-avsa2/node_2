a5bb1d72e1047286c439d074da022ffdcaf13f66
george.avsa <crazzylama0@yandex.ru>
2024-09-05 21:02:36 +0200

    added async file reader function with error catcher

---
+++ helpers/readFilePromise.js
@@ -0,0 +1,20 @@
+const fs = require("fs");
+
+function readFilePromise(path, encoding) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(path, encoding, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        try {
+          const stringifiedData = JSON.stringify(data.toString());
+          resolve(stringifiedData);
+        } catch (e) {
+          reject(e);
+        }
+      }
+    });
+  });
+}
+
+module.exports = readFilePromise;
--- index.js
+++ index.js
@@ -1,14 +1,71 @@
 const http = require("http");
+const path = require("path");
+const readFilePromise = require("./helpers/readFilePromise");
 
 const hostname = "127.0.0.1";
 const port = 3000;
 
+const handlers = {
+  "/sum": sum,
+};
+
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
-  res.end("Hello World");
+  parseBodyJson(req, (err, payload) => {
+    const handler = getHandler(req.url);
+
+    handler(req, res, payload, (err, result) => {
+      if (err) {
+        res.statusCode = err.code;
+        res.setHeader("Content-Type", "application/json");
+        res.end(JSON.stringify(err));
+
+        return;
+      }
+
+      res.statusCode = 200;
+      res.setHeader("Content-Type", "application/json");
+      res.end(JSON.stringify(result));
+    });
+  });
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+async function startServer() {
+  const articles = await readFilePromise(path.join(__dirname, "data", "articles.json"));
+  const comments = await readFilePromise(path.join(__dirname, "data", "comments.json"));
+
+  server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+  });
+}
+
+function getHandler(url) {
+  return handlers[url] || notFound;
+}
+
+function sum(req, res, payload, cb) {
+  const result = { c: payload.a + payload.b };
+
+  cb(null, result);
+}
+
+function notFound(req, res, payload, cb) {
+  cb({ code: 404, message: "Not found" });
+}
+
+function parseBodyJson(req, cb) {
+  let body = [];
+
+  req
+    .on("data", function (chunk) {
+      body.push(chunk);
+    })
+    .on("end", function () {
+      body = Buffer.concat(body).toString();
+
+      let params = JSON.parse(body);
+
+      cb(null, params);
+    });
+}
+
+startServer();
